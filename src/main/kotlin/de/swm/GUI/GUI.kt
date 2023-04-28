43921b0628d85f5e448cc06f69f0801adcd98ba5
HerrMaximus <m.loenker@icloud.com>
2023-04-28 16:03:57 +0200

    Restart
    Delete code, i change the layout

--- src/main/kotlin/de/swm/GUI/GUI.kt
+++ src/main/kotlin/de/swm/GUI/GUI.kt
@@ -1,4 +1,4 @@
-package de.swm.GUI
+package de.swm.gui
 
 
 import com.formdev.flatlaf.FlatDarculaLaf
@@ -10,51 +10,22 @@ class GUI {
     val username = JTextField()
     val message = JTextField()
     val listOfMessages = JList<String>()
+class GUI : JFrame() {
 
-    /*
-    - Input für den eigenen Nutzernamen am anfang
-    - Input für die Chatnachricht
-    - Button zum Absenden dieser Nachricht
-    - Liste von allen Chatnachrichten
-     */
+    private val usernameField = JTextField()
+    private val messageField = JTextField("Enter your message")
+    private var messageList = JList<String>()
 
     init {
-        UIManager.setLookAndFeel(FlatDarculaLaf())
-        JFrame.setDefaultLookAndFeelDecorated(true)
-        buildGUI(createGUI())
-    }
-    private fun createGUI(): JFrame {
-        val frame = JFrame("MQTT-Client")
-        frame.setSize(1000, 1000)
-        frame.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-        frame.layout = GridLayout(2, 1)
-        return frame
+        createUI()
     }
 
-    private fun buildGUI(frame: JFrame) {
-        val c = GridBagConstraints(); c.fill = GridBagConstraints.BOTH
 
-        val usernameLbl = JLabel("Your username:")
-        c.gridx = 0; c.gridy = 0
-        frame.add(usernameLbl, c)
 
-        c.gridx = 1; c.gridy = 0
-        frame.add(username, c)
 
-        c.gridx = 0; c.gridy = 1
-        frame.add(message, c)
 
-        val messageBt = JButton("Send message")
-        c.gridx = 1; c.gridy = 1
-        frame.add(messageBt, c)
 
-        c.gridx = 2; c.gridy = 0
-        frame.add(listOfMessages, c)
 
-        val listScroller = JScrollPane(listOfMessages)
-        c.gridx = 2; c.gridy = 1
-        frame.add(listScroller, c)
 
-        frame.isVisible = true
     }
 }
