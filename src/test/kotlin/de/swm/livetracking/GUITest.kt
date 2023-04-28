75e663f06e77a46596f3bab18d086526ef6ca515
HerrMaximus <m.loenker@icloud.com>
2023-04-28 15:09:10 +0200

    Create GUI Test

---
+++ src/test/kotlin/de/swm/livetracking/GUITest.kt
@@ -0,0 +1,11 @@
+package de.swm.livetracking
+
+import de.swm.GUI.GUI
+import org.junit.jupiter.api.Test
+
+class GUITest {
+    @Test
+    fun `Create and build the GUI`() {
+        GUI()
+    }
+}
