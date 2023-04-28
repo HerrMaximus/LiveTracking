cecb3f887ae12a4fb129aaee8623160e637fefd9
HerrMaximus <m.loenker@icloud.com>
2023-04-28 15:53:55 +0200

    Add invokeLater

--- src/test/kotlin/de/swm/livetracking/GUITest.kt
+++ src/test/kotlin/de/swm/livetracking/GUITest.kt
@@ -2,10 +2,13 @@ package de.swm.livetracking
 
 import de.swm.GUI.GUI
 import org.junit.jupiter.api.Test
+import javax.swing.SwingUtilities
 
 class GUITest {
     @Test
     fun `Create and build the GUI`() {
-        GUI()
+        SwingUtilities.invokeLater {
+            GUI()
+        }
     }
 }
