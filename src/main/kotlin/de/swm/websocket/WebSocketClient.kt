e402764e702c7d43f4aec89aa48cc4728cb2ce22
HerrMaximus <m.loenker@icloud.com>
2023-05-09 12:16:19 +0200

    Deleted unnecessary data class Response and changed login to make it shorter

--- src/main/kotlin/de/swm/websocket/WebSocketClient.kt
+++ src/main/kotlin/de/swm/websocket/WebSocketClient.kt
@@ -3,31 +3,22 @@ package de.swm.websocket
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
-
-data class Response(
-    val type: String? = null,
-    val title: String? = null,
-    val status: String? = null,
-    val detail: String? = null,
-    val instance: String? = null
-)
+import org.springframework.web.reactive.function.client.bodyToMono
 
 class WebSocketClient {
 
     fun login(username: String, password: String): Boolean {
-        val client = WebClient.builder()
-            .baseUrl("http://mischiefsmp.com:8080")
-            .build()
-        return client.post().uri { uriBuilder ->
-            uriBuilder.path("/join")
-            uriBuilder.queryParam("username", username)
-            uriBuilder.queryParam("password", password).build()
-        }
+        val client = WebClient.create("http://mischiefsmp.com:8080")
+        return client.post()
+            .uri("/join") {
+                it.queryParam("username", username)
+                    .queryParam("password", password)
+                    .build()
+            }
             .retrieve()
-            .bodyToMono(Response::class.java)
-            .map { response ->
-                response.title == "OK"
-            }.block() ?: false
+            .bodyToMono<String>()
+            .map { it.contains("\"title\":\"OK\"") }
+            .block() ?: false
     }
 
     fun sendMessage(username: String, password: String, message: String) {
